ff427db8941b7be5d8699d1f95e5da81516ab896
James <logicism81@gmail.com>
2024-04-15 10:22:32 -0400

    Dark colors, logo, .env

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
       title: "Blueprint",
       logo: {
         alt: "Blueprint logo",
-        src: "img/logo.svg",
+        src: "img/logo.png",
       },
       items: [
         {
